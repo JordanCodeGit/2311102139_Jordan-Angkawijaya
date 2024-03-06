5fae52f6303508da64b67bf1b3ec875ba24b056f
Jordan <89541564+foxcodess@users.noreply.github.com>
2024-03-06 16:23:41 +0700

    Modul 1

---
+++ Pertemuan01/Guided1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch (op)
+    {
+    case '+':
+        cout << "Result: " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "Result: " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "Result: " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 == 0) {
+            cout << "Result: " << fixed << setprecision(2) << num1 / num2 << endl;
+        } else {
+            cout << "Error!: Division by zero" << endl;
+        }
+        break;
+
+    default:
+        cout << "Error! operator is not correct" << endl;
+        break;
+    }
+    return 0;
+}
